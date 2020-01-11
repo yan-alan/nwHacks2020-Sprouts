67ab0e168d030d1ad64be005ec7d597fa5ad594e
Alan Yan <yanalan7@gmail.com>
2020-01-11 15:10:24 -0800

    Setup Scene Delegate

---
+++ Sprouts/Sprouts/Controllers/TerrariumViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TerrariumViewController.swift
+//  Sprouts
+//
+//  Created by Alan Yan on 2020-01-11.
+//  Copyright © 2020 Alan Yan. All rights reserved.
+//
+
+import UIKit
+
+class TerrariumViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ Sprouts/Sprouts/Views/TerrariumView.swift
@@ -0,0 +1,9 @@
+//
+//  TerrariumView.swift
+//  Sprouts
+//
+//  Created by Alan Yan on 2020-01-11.
+//  Copyright © 2020 Alan Yan. All rights reserved.
+//
+
+import Foundation
