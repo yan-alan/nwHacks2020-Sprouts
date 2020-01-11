8f205d3a876495379e9ec411f5d20e8b0cbe92b4
Alan Yan <yanalan7@gmail.com>
2020-01-11 15:06:04 -0800

    boiler plate of sprouts

---
+++ Sprouts/Sprouts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sprouts
+//
+//  Created by Alan Yan on 2020-01-11.
+//  Copyright © 2020 Alan Yan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
