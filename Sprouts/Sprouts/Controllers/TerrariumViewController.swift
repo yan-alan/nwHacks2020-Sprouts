d12083dcbdfe3f243ad4d344588fb0c05d93aec3
Alan Yan <yanalan7@gmail.com>
2020-01-12 10:00:33 -0800

    Update TerrariumViewController.swift

--- Sprouts/Sprouts/Controllers/TerrariumViewController.swift
+++ Sprouts/Sprouts/Controllers/TerrariumViewController.swift
@@ -70,13 +70,11 @@ class TerrariumViewController: UIViewController {
         toEdit = !toEdit
     }
     func handleMyBars() {
-        if(plants.count >= 3) {
-            for i in 1...(plants.count/3+1) {
-                let imageView = ContentFitImageView(frame: CGRect(x: 0, y: (i*165 + (i-1)*30 - 2), width: (Int(UIScreen.main.bounds.size.width-70)), height: 14))
-        
-                imageView.image = UIImage(named: "shelf")
-                collectionView.addSubview(imageView)
-            }
+        for i in 1...(plants.count/3+1) {
+            let imageView = ContentFitImageView(frame: CGRect(x: 0, y: (i*165 + (i-1)*30 - 2), width: (Int(UIScreen.main.bounds.size.width-70)), height: 14))
+    
+            imageView.image = UIImage(named: "shelf")
+            collectionView.addSubview(imageView)
         }
     }
     @objc func handleLongGesture(gesture: UILongPressGestureRecognizer) {
