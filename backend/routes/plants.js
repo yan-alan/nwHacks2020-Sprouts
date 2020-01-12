a6313ddaf345db0072a68c7d7e6918f40afdbdf4
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-01-11 21:26:59 -0800

    Add stubs for /plants endpoint

---
+++ backend/routes/plants.js
@@ -0,0 +1,26 @@
+import { database } from "../app";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", function(res, req, next) {
+    
+});
+
+router.post("/", function(res, req, next) {
+    
+});
+
+router.put("/", function(res, req, next) {
+    
+});
+
+router.delete("/", function(res, req, next) {
+    
+});
+
+router.get("/:id", function(res, req, next) {
+    
+});
+
+
