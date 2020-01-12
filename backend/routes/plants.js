cb50f4061bb8048b96130300da19f057fe216a7a
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-01-11 22:24:15 -0800

    Add POST /plants implementation

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -3,24 +3,54 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", function(res, req, next) {
+const dbName = "Plants";
+const collectionName = "PlantCollection";
+
+router.get("/", function(req, res, next) {
     
 });
 
-router.post("/", function(res, req, next) {
+router.post("/", function(req, res, next) {
+    const { username } = req.body;
+
+    // TODO: extract other properties of plants to save to database
+    const { plant } = req.body;
+
+    database.open(dbName, collectionName)
+        .then(collection => {
+            return collection.updateOne(
+                { username: username },
+                { $push: { plants: plant } }
+            );
+        })
+        .then(writeResult => {
+            const nModified = writeResult.result.nModified;
+            if (nModified > 0) {
+                return res.status(200).send({ 
+                    message: "Plant was successfully added.", 
+                    plant: plant
+                }).send();
+            } else {
+                console.log("here");
+                return res.status(422).send({ error: "Could not add plant. Please try again." });
+            }
+        })
+        .catch(err => {
+            res.status(404).send({ error: err });
+        });
+});
+
+router.put("/", function(req, res, next) {
     
 });
 
-router.put("/", function(res, req, next) {
+router.delete("/", function(req, res, next) {
     
 });
 
-router.delete("/", function(res, req, next) {
+router.get("/:id", function(req, res, next) {
     
 });
 
-router.get("/:id", function(res, req, next) {
-    
-});
-
+export { router };
 
