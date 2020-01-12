72cf0d94dd36a0198544613675bc304a60542b4a
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-01-12 01:52:03 -0800

    Add documentation for /plants endpoints

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -6,6 +6,13 @@ const router = express.Router();
 const dbName = "Plants";
 const collectionName = "PlantCollection";
 
+/**
+ * GET /api/plants/
+ * 
+ * @param {string} username - the user's username
+ * 
+ * @returns (200) plants - the array of plants associated with a user
+ */
 router.get("/", function(req, res, next) {
     const { username } = req.body;
 
@@ -15,6 +22,14 @@ router.get("/", function(req, res, next) {
         .catch(err => res.status(404).send({ error: err }));
 });
 
+/**
+ * POST /api/plants/
+ * 
+ * @param {string} username - the user's username
+ * @param {Plant} plant - the plant object to record
+ * 
+ * @returns (201) plant - the plant object that was successfully saved
+ */
 router.post("/", function(req, res, next) {
     const { plant, username } = req.body;
 
@@ -27,7 +42,7 @@ router.post("/", function(req, res, next) {
         })
         .then(writeResult => {
             if (writeResult.matchedCount > 0) {
-                return res.status(200).send({ 
+                return res.status(201).send({ 
                     message: "Plant was successfully added.", 
                     plant: plant
                 });
@@ -40,6 +55,14 @@ router.post("/", function(req, res, next) {
         .catch(err => res.status(404).send({ error: err }));
 });
 
+/**
+ * PUT /api/plants
+ * 
+ * @param {string} username - the user's username
+ * @param {Plant} plant - the plant object to be modified
+ * 
+ * @returns (200) plants - the array of plants associated with a user, after modifying the target plant
+ */
 router.put("/", function(req, res, next) {
     const { username, plant } = req.body;
 
@@ -73,6 +96,14 @@ router.put("/", function(req, res, next) {
         .catch(err => res.status(404).send({ error: err }));
 });
 
+/**
+ * DELETE /api/plants
+ * 
+ * @param {string} username - the user's username
+ * @param {number} plantId - the id of the plant to be deleted
+ * 
+ * @returns (200) plants - the array of plants associated with a user, after deleting the target plant
+ */
 router.delete("/", function(req, res, next) {
     const { username, plantId } = req.body;
 
@@ -112,6 +143,13 @@ router.delete("/", function(req, res, next) {
         .catch(err => res.status(404).send({ error: err }));
 });
 
+/**
+ * GET /api/plants/:id
+ * 
+ * @param {string} username - the user's username
+ * 
+ * @returns (200) plant - the plant object matching the given plantId
+ */
 router.get("/:id", function(req, res, next) {
     const plantId = Number(req.params.id);
     const { username } = req.body;
