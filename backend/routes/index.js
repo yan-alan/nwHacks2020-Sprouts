858012434c0f582283309a27dfa91a0d230c37dc
Andrew Zulaybar <andrewzulaybar@gmail.com>
2020-01-11 17:58:44 -0800

    Add username and password checks on user

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,17 +4,26 @@ import express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const { username } = req;
+router.get("/", function(req, res, next) {
+  const { username, password } = req.body;
 
-  database.open("Plants", "PlantCollection")
-    .then((collection) => {
-      collection.insertOne({ username: username });
-      res.json({ message: "Success!" });
+  if (username === undefined || password === undefined) {
+    res.status(422).send({ error: "Username or password must be specified"});
+  } else {
+    database.open("Plants", "PlantCollection")
+    .then(collection => collection.find({ username, password }))
+    .then(cursor => cursor.count())
+    .then(count => {
+      if (count) {
+        res.status(201).json({ message: "Successfully logged in!" }).send();
+      } else {
+        res.status(201).json({ error: "Could not find a user that matches the given username or password!" });
+      }
     })
     .catch((err) => {
       res.status(422).send({ error: err });
-    })
+    });
+  }
 });
 
 export { router }; 
